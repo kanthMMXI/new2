a071e1ec4d076b64387de94fb235122ad6d5a30e
kanthMMXI <kanthrfp@gmail.com>
2025-02-11 10:56:21 +0100

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,22 +1,24 @@
-/* translate.js */
-async function translateText() {
-    let text = document.getElementById('textInput').value;
-    let targetLang = document.getElementById('languageSelect').value;
-    let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=it&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`;
-    
-    let response = await fetch(url);
-    let result = await response.json();
-    let translatedText = result[0].map(item => item[0]).join(' ');
-    document.getElementById('outputText').innerText = translatedText;
-}
-
 function speakTranslation() {
     let translatedText = document.getElementById('outputText').innerText;
     if (!translatedText) {
         alert("No translated text to speak!");
         return;
     }
+
+    let langCode = document.getElementById('languageSelect').value;
+    let speechLangMap = {
+        "si": "si-LK",  // Sinhala (Sri Lanka)
+        "ta": "ta-IN",  // Tamil (India)
+        "tl": "tl-PH"   // Tagalog (Philippines)
+    };
+
     let utterance = new SpeechSynthesisUtterance(translatedText);
-    utterance.lang = document.getElementById('languageSelect').value;
-    speechSynthesis.speak(utterance);
+    utterance.lang = speechLangMap[langCode] || langCode;
+
+    console.log("Speaking:", translatedText, "in", utterance.lang);
+
+    // Small delay to fix Chrome issues
+    setTimeout(() => {
+        speechSynthesis.speak(utterance);
+    }, 100);
 }
